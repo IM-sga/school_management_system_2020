9f4b97705b4710d8abebbcdffebab5b5734f13cf
Irish Lejarde <hermesaniesteph@gmail.com>
2020-08-06 19:15:34 +0800

    IML.css deleted

--- index.php
+++ index.php
@@ -2,41 +2,46 @@
 <html>
 <head>
 	<title>IML Academy</title>
-	<!-- <link rel="stylesheet" type="text/css" href="public/css/IML.css"> -->
-
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" type="text/css" href="public/css/bootstrap.min.css">
-	<script href="public/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="public/css/index.css">
 </head>
 <body>
-	<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-success">
-  <a class="navbar-brand" href="#">IML Academy</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarCollapse">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="pages/chooseRole.php">Sign Up <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="pages/imlogin.php">Log In</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Enroll Now</a>
-      </li>
-    </ul>
-  </div>
-</nav>
+	<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+		<a class="navbar-brand" href="index.php">IML Academy</a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse" id="navbarCollapse">
+			<ul class="navbar-nav mr-auto">
+				<li class="nav-item">
+					<a class="nav-link" href="#">About us</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#">Campus Branches</a>
+				</li>
+			</ul>
+			<a class="btn btn-outline-success mr-1" role="button" href="pages/imlogin.php">Log In</a>
+			<a class="btn btn-outline-warning mr-1" role="button" href="pages/chooseRole.php">Sign Up</a>
+		</div>
+	</nav>
 
+	<div class="jumbotron">
+		<div class="container">
+			<h1 class="display-3"> IML Academy</h1>
+			<p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
+			<p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more &raquo;</a></p>
+		</div>
+	</div>
+	
 	<div id= "main-art">
 		<h2 class="page-header">LOREM IPSUM</h2>
 		<p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 		tempor incididunt ut labore et dolore magna aliqua.</p>
 		<p class="text-justify">
-		 Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodoconsequat. Duis aute irure dolor in reprehenderit in voluptate velit essecillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat nonproident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>	
+		Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodoconsequat. Duis aute irure dolor in reprehenderit in voluptate velit essecillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat nonproident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>	
 	</div>
 	<div>
 		<h3>DOLOR SIT</h3>
@@ -44,5 +49,9 @@
 		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
 		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commod.</p>
 	</div>
+</div>
+
+
+<script href="public/js/bootstrap.min.js"></script>
 </body>
 </html>
--- pages/chooseRole.php
+++ pages/chooseRole.php
@@ -2,15 +2,15 @@
 <title>IML Registration Page</title>
 <?php require '../partials/header.php'; ?>
 <body>
-	<div>
+	<div class="container"> 
 		<h2>Choose your role:</h2>
 
 		<div class="container">
 			<a class="btn btn-primary" href="studentRegistration.php">I'm a Student</a>
 		</div>
-		<br>
+		
 		<div class="container">
-			<a class="btn btn-danger" href="teacherRegistration.php">I'm a Teacher</a>
+			<a class="btn btn-danger"  href="teacherRegistration.php">I'm a Teacher</a>
 		</div>
 	</div>
 <?php require '../partials/footer.php'; ?>
