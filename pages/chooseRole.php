43643a40568c74acb303fcb0386daaf422d60ab2
Irish Lejarde <hermesaniesteph@gmail.com>
2020-08-12 18:06:22 +0800

    page modified

--- pages/chooseRole.php
+++ pages/chooseRole.php
@@ -4,13 +4,10 @@
 <body>
 	<div>
 		<h2>Choose your role:</h2>
-
-		<div class="container">
-			<a class="btn btn-primary" href="studentRegistration.php">I'm a Student</a>
-		</div>
-		<br>
-		<div class="container">
-			<a class="btn btn-danger" href="teacherRegistration.php">I'm a Teacher</a>
+		<div class="container"> 
+			<a class="pull-left btn btn-primary" href="studentRegistration.php">I'm a Student</a>
+			<a class="pull-right btn btn-danger" href="teacherRegistration.php">I'm a Teacher</a>
 		</div>
 	</div>
+	
 <?php require '../partials/footer.php'; ?>
