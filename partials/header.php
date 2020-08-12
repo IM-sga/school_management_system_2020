a933699ff00b6793217ef0a33ec2f3b3325c3fe0
Irish Lejarde <hermesaniesteph@gmail.com>
2020-08-12 18:22:25 +0800

    header modified

--- partials/header.php
+++ partials/header.php
@@ -1,6 +1,6 @@
 </head>
 <header> 
 	<nav class="navbar navbar-expand-md navbar-dark sticky-top bg-dark">
-		<a class="navbar-brand" href="../index.php"><img class="img-fluid p-1" src="public/assets/images/logo6-2.png"></a>
+		<a class="navbar-brand" href="../index.php"><img class="img-fluid p-1" src="../public/assets/images/logo6-2.png"></a>
 	</nav>
 </header>
