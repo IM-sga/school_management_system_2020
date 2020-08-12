ab6718ad707e38d573e518c72ee14da5c235278d
Irish Lejarde <hermesaniesteph@gmail.com>
2020-08-12 18:00:39 +0800

    registration forms updated

--- pages/studentRegistration.php
+++ pages/studentRegistration.php
@@ -2,77 +2,86 @@
 <title>IML Registration Page</title>
 <?php require '../partials/header.php'; ?>
 <body>
-	<form method="POST" action="../forms/studentRegistration.php">
-        <div>
-        	<label for="Last name:">Last name: </label>
-        	<input type="text" name="lname" required>
-        </div>
-        <div>
-        	<label for="First name">First name: </label>
-        	<input type="text" name="fname"required> 
-        </div>
-       	<div>
-       		<label for="Middle name">Middle name: </label>
-			<input type="text" name="mname"required>
-       	</div> 
-       	<div>
-       		<label for="Name extension">Name extension: </label>
-			<input type="text" name="nameext">
-       	</div>
-       	<div>
-       		<label for="Username">Username: </label>
-			<input type="text" name="username"required> 	
-       	</div>
-       	<div>
-       		<label for="Birthdate">Birthdate: </label>
-			<input type="date" name="bdate"required>
-       	</div>
-        <div>
-        	<label for="Sex">Sex: </label>
-			<select class="sex" name="sex" required>
-				<option value="female"> Female </option>
-				<option value="male"> Male </option>
-				<option value="non-binary"> Non-binary </option>
-			</select>
-		</div>
-		<div>
-			<label for="Email">Email: </label>
-        	<input type="email" name="email"required>
-		</div>
-        <div>
-        	<label for="Contact number">Contact number: </label>
-	        <input type="text" name="cnumber"required> 
-        </div>
-		<div>
-			<label for="Address">Address: </label>
-	        <input type="text" name="address"required> 
-		</div>
-		<div>
-			<label for="Guardian's name">Guardian's name: </label>
-        	<input type="text" name="gname"required>
-		</div>
-		<div>
-			<label for="Relationship to student"required>Relationship to student: </label>
-	        <input type="text" name="relstu"required>
-		</div>
-		<div>
-			<label for="Guardian's contact number">Guardian's contact number: </label>
-	        <input type="text" name="gnumber"required>
-		</div>
-		<div>
-			<label for="Guardian's address: "></label>
-	        <input type="text" name="gadd"required>
-		</div>
-		<div>
-			<label for="Password">Password: </label>
-	        <input type="password" name="password"required>
-		</div>
-		<div>
-			<label for="Confirm password">Confirm password: </label>
-        	<input type="password" name="cpassword"required>
-		</div>
-		<div>
-			<input cass="btn btn-primary" type="submit" name="submitBtn" value="Submit">
-		</div>
-	</form>
+	<div class="container mt-5 mb-5" id="stuReg">
+		<form method="POST" action="../forms/studentRegistration.php">
+			<div class="p-2 pt-3">
+				<h1 id="f-back">PERSONAL BACKGROUND</h1>
+			</div>
+			<div class="p-2">
+				<label for="Last name:">Last name: </label>
+				<input class="form-control" type="text" name="lname" required>
+			</div>
+			<div class="p-2">
+				<label for="First name">First name: </label>
+				<input class="form-control" type="text" name="fname"required> 
+			</div>
+			<div class="p-2">
+				<label for="Middle name">Middle name: </label>
+				<input class="form-control" type="text" name="mname"required>
+			</div> 
+			<div class="p-2">
+				<label for="Name extension">Name extension: </label>
+				<input class="form-control" type="text" name="nameext">
+			</div>
+			<div class="p-2">
+				<label for="Username">Username: </label>
+				<input class="form-control" type="text" name="username"required> 	
+			</div>
+			<div class="p-2">
+				<label for="Birthdate">Birthdate: </label>
+				<input class="form-control" type="date" name="bdate"required>
+			</div>
+			<div class="p-2">
+				<label for="Sex">Sex: </label>
+				<select class="form-control" name="sex" required>
+					<option value="female"> Female </option>
+					<option value="male"> Male </option>
+					<option value="non-binary"> Non-binary </option>
+				</select>
+			</div>
+			<div class="p-2">
+				<label for="Email">Email: </label>
+				<input class="form-control" type="email" name="email"required>
+			</div>
+			<div class="p-2">
+				<label for="Contact number">Contact number: </label>
+				<input class="form-control" type="text" name="cnumber"required> 
+			</div>
+			<div class="p-2">
+				<label for="Address">Address: </label>
+				<input class="form-control" type="text" name="address"required> 
+			</div>
+			<div class="p-2">
+				<h1 id="f-back">GUARDIAN'S BACKGROUND</h1>
+			</div>
+			<div class="p-2">
+				<label for="Guardian's name">Guardian's name: </label>
+				<input class="form-control" type="text" name="gname"required>
+			</div>
+			<div class="p-2">
+				<label for="Relationship to student"required>Relationship to student: </label>
+				<input class="form-control" type="text" name="relstu"required>
+			</div>
+			<div class="p-2">
+				<label for="Guardian's contact number">Guardian's contact number: </label>
+				<input class="form-control" type="text" name="gnumber"required>
+			</div>
+			<div class="p-2">
+				<label for="Guardian's address:">Guardian's address:</label>
+				<input class="form-control" type="text" name="gadd"required>
+			</div>
+			<div class="p-2">
+				<label for="Password">Password: </label>
+				<input class="form-control" type="password" name="password"required>
+			</div>
+			<div class="p-2 pb-3">
+				<label for="Confirm password">Confirm password: </label>
+				<input class="form-control" type="password" name="cpassword"required>
+			</div>
+			<div class="p-2 pb-5">
+				<input class="form-control btn btn-dark" type="submit" name="submitBtn" value="Submit">
+			</div>
+		</form>
+	</div>
+	
 <?php require '../partials/footer.php'; ?>
--- pages/teacherRegistration.php
+++ pages/teacherRegistration.php
@@ -2,41 +2,44 @@
 <title>IML Registration Page</title>
 <?php require '../partials/header.php'; ?>
 <body>
-	<form method="POST" action="../forms/teacherRegistration.php">
-    <div>
-    	<label for="Last name:">Last name: </label>
-    	<input type="text" name="lname" required>
-    </div>
-    <div>
-    	<label for="First name">First name: </label>
-    	<input type="text" name="fname"required> 
-    </div>
-   	<div>
-   		<label for="Middle name">Middle name: </label>
-	    <input type="text" name="mname"required>
-   	</div> 
-   	<div>
-   		<label for="Birthdate">Birthdate: </label>
-	    <input type="date" name="bdate"required>
-   	</div> 
-   	<div>
-   		<label for="Username">Username: </label>
-	    <input type="text" name="username"required> 	
-   	</div>
-   	<div>
-	    <label for="Email">Email: </label>
-    	<input type="email" name="email"required>
-    </div>
-    <div>
-	   <label for="Password">Password: </label>
-      <input type="password" name="password"required>
-    </div>
-    <div>
-	    <label for="Confirm password">Confirm password: </label>
-    	<input type="password" name="cpassword"required>
-    </div>
-    <div>
-	    <input class="btn btn-primary" type="submit" name="submitBtn" value="Submit">
-    </div>
-	</form>
+	<div class="container mt-5 mb-5" id="teaReg">
+		<form method="POST" action="../forms/teacherRegistration.php">
+			<div class="p-2 pt-3">
+				<label for="Last name:">Last name: </label>
+				<input class="form-control" type="text" name="lname" required>
+			</div>
+			<div class="p-2">
+				<label for="First name">First name: </label>
+				<input class="form-control" type="text" name="fname"required> 
+			</div>
+			<div class="p-2">
+				<label for="Middle name">Middle name: </label>
+				<input class="form-control" type="text" name="mname"required>
+			</div> 
+			<div class="p-2">
+				<label for="Birthdate">Birthdate: </label>
+				<input class="form-control" type="date" name="bdate"required>
+			</div> 
+			<div class="p-2">
+				<label for="Username">Username: </label>
+				<input class="form-control" type="text" name="username"required> 	
+			</div>
+			<div class="p-2">
+				<label for="Email">Email: </label>
+				<input class="form-control" type="email" name="email"required>
+			</div>
+			<div class="p-2">
+				<label for="Password">Password: </label>
+				<input class="form-control" type="password" name="password"required>
+			</div>
+			<div class="p-2">
+				<label for="Confirm password">Confirm password: </label>
+				<input class="form-control" type="password" name="cpassword"required>
+			</div>
+			<div class="mt-3 p-2 pb-5">
+				<input class="form-control btn btn-dark" type="submit" name="submitBtn" value="Submit">
+			</div>
+		</form>
+	</div>
+	
 <?php require '../partials/footer.php'; ?>
