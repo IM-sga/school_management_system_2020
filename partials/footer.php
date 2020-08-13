7d6de7e6a1cdeda0b9bc842ebc09e12e64a7870f
Irish Lejarde <hermesaniesteph@gmail.com>
2020-08-13 22:02:55 +0800

    footer created

--- partials/footer.php
+++ partials/footer.php
@@ -1,2 +1,7 @@
+<footer class="container">
+    <p class="float-right"><a href="#">Back to top</a></p>
+    <p>&copy; 2020 IML Academy &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+</footer>
+
 </body>	
 </html>
