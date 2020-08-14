69f102bbb66734de9c26f282e6edc524f88512db
Irish Lejarde <hermesaniesteph@gmail.com>
2020-08-14 21:34:15 +0800

    navbar & nav-link modified

--- index.php
+++ index.php
@@ -7,13 +7,13 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" type="text/css" href="public/css/bootstrap.min.css">
 	<script href="public/js/bootstrap.min.js"></script>
-	<link rel="stylesheet" type="text/css" href="public/css/index.css">
+	<link rel="stylesheet" type="text/css" href="public/css/style.css">
 
 </head>
 <body>
 	
 	<!-- Sticky navbar -->
-	<nav class="navbar navbar-expand-md navbar-dark bg-dark sticky-top">
+	<nav class="navbar navbar-expand-md navbar-dark bg-dark">
 		<a class="navbar-brand" href="index.php"><img class="img-fluid p-1" src="public/assets/images/logo6-2.png"></a>
 		<button class="navbar-toggler" 
 				type="button" 
@@ -27,49 +27,42 @@
 		<div class="collapse navbar-collapse" id="navbarResponsive">
 			<ul class="navbar-nav mr-auto">
 				<li class="nav-item">
-					<a class="nav-link btn-sm" href="#">Campus Branches</a>
+					<a class="nav-link" href="#">Campus Branches</a>
 				</li>
 				<li class="nav-item">
-					<a class="nav-link btn-sm" href="#">Contact</a>
+					<a class="nav-link" href="#">Contact</a>
 				</li>
 			</ul>
 			<ul class="navbar-nav ml-auto">
 				<li class="nav-item">
-					<a href="pages/imlogin.php" class="btn btn-outline-success btn-sm ml-2 my-sm-0">Log In</a>
+					<a href="pages/imlogin.php" class="btn btn-outline-success ml-2 my-sm-0">Log In</a>
 				</li>
 				<li class="nav-item">
-					<a href="pages/chooseRole.php" class="btn btn-outline-warning btn-sm ml-2 my-sm-0">Sign Up</a>
+					<a href="pages/chooseRole.php" class="btn btn-outline-warning ml-2 my-sm-0">Sign Up</a>
 				</li>
 			</ul>
 		</div>
 	</nav>
 	
-	<!-- Image slider(Carousel) with Website's About and Tag lines  -->
-	<div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
-		<div class="carousel-inner">
-			<div class="carousel-item active">
-				<img src="public/assets/images/background.jpg" class="d-block w-100" alt="b">
-			</div>
-			<div class="carousel-item">
-				<img src="public/assets/images/background2.jpg" class="d-block w-100" alt="b2">
-			</div>
-			<div class="carousel-item">
-				<img src="public/assets/images/background3.jpg" class="d-block w-100" alt="b3">
-			</div>
+	
+	<!-- Jumbotron -->
+	<div class="jumbotron jumbotron-fluid">
+		<div class="container">
+			<h1 class="display-4">IML Academy</h1>
+			<p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+			<hr class="my-4">
+			<p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+			<p class="lead">
+				<a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+			</p>
 		</div>
-		<a class="carousel-control-prev" href="#carouselExampleSlidesOnly" role="button" data-slide="prev">
-			<span class="carousel-control-prev-icon" aria-hidden="true"></span>
-			<span class="sr-only">Previous</span>
-		</a>
-		<a class="carousel-control-next" href="#carouselExampleSlidesOnly" role="button" data-slide="next">
-			<span class="carousel-control-next-icon" aria-hidden="true"></span>
-			<span class="sr-only">Next</span>
-		</a>
 	</div>
 
 
 
-	<!-- Jumbotron -->
+
+
+	<!-- content -->
 	<div class="mt-5">
 		<h2 class="page-header">LOREM IPSUM</h2>
 		<p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
