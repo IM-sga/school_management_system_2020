36dce8b27318cd18e9192e8c8fb830a6f0b3c21f
Irish Lejarde <hermesaniesteph@gmail.com>
2020-08-15 23:16:09 +0800

    card created

--- pages/chooseRole.php
+++ pages/chooseRole.php
@@ -2,22 +2,28 @@
 <title>IML Registration Page</title>
 <?php require '../partials/header.php'; ?>
 <body>
-	<div>
+	<div class="container mt-5">
 		<h2>Choose your role:</h2>
-		<div class="container"> 
-			<ul class="list-unstyled w-30 p-5 mr-auto">
-				<li>
-					<a class="pull-left list-group-item-primary" href="studentRegistration.php">I'm a Student</a>
-				</li>
-			</ul>
-			<ul class="list-unstyled w-25 p-5 pull ml-auto">
-				<li>
-					<a class="pull-right list-group-item-danger" href="teacherRegistration.php">I'm a Teacher</a>
-				</li>
-			</ul>
-			
-			
-		</div>
 	</div>
-	
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6 col-sm-6 col-xs-12">
+				<div class="card p-4 bg-warning mr-auto" style="width: 18rem;">
+					<img class="card-img-top" src="../public/assets/images/student.png" alt="Student reading a books. PNG file">
+					<div class="card-body">
+						<a class="pull-left btn btn-primary btn-block" href="studentRegistration.php">I'm a Student</a>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-6 col-sm-6 col-xs-12">
+				<div class="card p-4 bg-info ml-auto" style="width: 18rem;">
+						<img class="card-img-top" src="../public/assets/images/teacher2.png" alt="Teacher with eyeglasses. PNG file">
+						<div class="card-body">
+							<a class="pull-left btn btn-danger btn-block" href="teacherRegistration.php">I'm a Teacher</a>
+						</div>
+					</div>
+				</div>
+			</div>
+	</div>
+
 <?php require '../partials/footer.php'; ?>
