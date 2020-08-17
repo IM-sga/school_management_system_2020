ce92d2b3904ec734a7db8e9d9752680f9f6e4a5d
Irish Lejarde <hermesaniesteph@gmail.com>
2020-08-17 22:44:21 +0800

    navbar class edited

--- partials/header.php
+++ partials/header.php
@@ -1,6 +1,6 @@
 </head>
 <header> 
-	<nav class="navbar navbar-expand-md navbar-dark bg-light flex-md-nowrap shadow">
+	<nav class="navbar navbar-expand-md navbar-light bg-light sticky-top flex-md-nowrap shadow">
 		<a class="navbar-brand" href="../index.php"><img src="../public/assets/images/logo5.png"></a>
 	</nav>
 </header>
