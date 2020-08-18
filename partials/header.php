184e06be176b21dcc828d705d06d92b0222503ba
Irish Lejarde <hermesaniesteph@gmail.com>
2020-08-18 18:40:23 +0800

    img src edited

--- partials/header.php
+++ partials/header.php
@@ -1,6 +1,6 @@
 </head>
 <header> 
 	<nav class="navbar navbar-expand-md navbar-light bg-light sticky-top flex-md-nowrap shadow">
-		<a class="navbar-brand" href="../index.php"><img src="../public/assets/images/logo5.png"></a>
+		<a class="navbar-brand" href="../index.php"><img src="../public/assets/logo/logo.png"></a>
 	</nav>
 </header>
