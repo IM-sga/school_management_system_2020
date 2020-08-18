e8aeb5e882af4d50877c502d62fa8032870084ad
Irish Lejarde <hermesaniesteph@gmail.com>
2020-08-18 17:52:06 +0800

    footer modified

--- partials/footer.php
+++ partials/footer.php
@@ -1,5 +1,5 @@
 <hr class="display-4 mt-5">
-<footer class="container mt-5">
+<footer class="footer p-3 pt-2">
     <p class="float-right"><a href="#">Back to top</a></p>
     <p>&copy; 2020 IML Academy &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
 </footer>
