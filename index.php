6f889e969e39a49195828ff8eadbf141bd57e182
Irish Lejarde <hermesaniesteph@gmail.com>
2020-08-19 23:16:09 +0800

    2nd content item (features) modified

--- index.php
+++ index.php
@@ -69,7 +69,7 @@
 	<section id="features">
 		<div class="card-deck m-5">
 			<div class="card">
-				<img src="public/assets/images/final.jpg" class="card-img-top" alt="PDF icon. PNG file.">
+				<img src="public/assets/images/docs.jpg" class="card-img-top" alt="PDF icon. PNG file.">
 				<div class="card-body">
 					<h5 class="card-title">Card title</h5>
 					<p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
